02988c21c0d12dd82ad0929b0949cca4a2f512bb
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-16 13:56:10 +0200

    Add project files.

---
+++ 4.B Exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.B_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the first integer:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a >0)
+            {
+                Console.WriteLine(a % 3 == 0 || a % 7 == 0);
+            }
+        }
+    }
+}
